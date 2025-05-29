8498eee80e19428a7e323de8307bc962f0c644d0
Maximiliano Quiroga Errazuriz <m.quirogaerrzuriz@uandresbello.edu>
2025-05-29 01:07:32 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,27 @@
+// Element references
+let search = document.querySelector(".search-box");
+let menu = document.querySelector(".nav-links");
+let header = document.querySelector("header");
+
+// Toggle search box
+document.querySelector("#search-icon").onclick = () => {
+  search.classList.toggle("active");
+  menu.classList.remove("active");
+};
+
+// Toggle menu
+document.querySelector("#menu-icon").onclick = () => {
+  menu.classList.toggle("active");
+  search.classList.remove("active");
+};
+
+// Add shadow on scroll
+window.addEventListener("scroll", () => {
+  header.classList.toggle("shadow", window.scrollY > 0);
+});
+
+//Hide menu on scroll
+window.onscroll = () => {
+  menu.classList.remove("active");
+  search.classList.remove("active");
+};
